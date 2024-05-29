9bd3041617eba47e237edb675c079b874199a861
dachi giorgobiani <dachi.giorgobiani@meteor.be>
2024-05-29 17:00:46 +0200

    deleted nextauth, going to try new approach

--- crypto-hippo/src/app/sign-up/page.tsx
+++ crypto-hippo/src/app/sign-up/page.tsx
@@ -1,22 +1,9 @@
-import { prisma } from "@/lib/prisma";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-import { User } from "../user";
 
 export default async function SignUp() {
-  const session = await getServerSession(authOptions);
-  const user = await prisma.user.findFirst({
-    where: {
-      email: "test@test.com",
-    },
-  });
+ 
   return (
     <>
-      <p>hello, {user?.name}</p>
-      <p>SERVER</p>
-      <p>{JSON.stringify(session)}</p>
-      <p>client</p>
-      <User />
+   
     </>
   );
 }
