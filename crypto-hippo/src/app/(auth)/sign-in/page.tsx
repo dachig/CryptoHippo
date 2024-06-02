59b8909fd1f7b0c2b7defd5432e79d029247034b
dachi giorgobiani <dachi.giorgobiani@meteor.be>
2024-06-02 14:58:07 +0200

    feat: registration page correctly adds user to database with necessary error handling.

---
+++ crypto-hippo/src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import SignInForm from "@/components/form/signInForm";
+
+export default async function SignIn() {
+  return (
+    <div className="w-max-lg mx-auto">
+      <SignInForm />
+    </div>
+  );
+}
---
+++ crypto-hippo/src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignUpForm from "@/components/form/signUpForm";
+
+export default async function SignUp() {
+  return (
+    <div className="w-max-lg mx-auto">
+      <SignUpForm />
+    </div>
+  );
+}
